51656e3f08aba2d2d66b85c7bf4cb35a2ff2b84e
dafinoer <dafidzeko@gmail.com>
2018-04-24 11:42:18 +0700

    font size

--- js/base.js
+++ js/base.js
@@ -2,13 +2,15 @@ const myVue = new Vue({
     el:'#app',
     data:{
         time: null,
-        totalTime:(25 * 60),
+        totalTime:(1 * 60),
         resetButton:false
     },
     methods:{
         play:function(){
-           this.time = setInterval(() => this.countdown(), 1000);
-           this.resetButton = true
+
+            this.time = setInterval(() => this.countdown(), 1000);
+            this.resetButton = true
+
         },
         countdown:function(){
 
@@ -16,12 +18,15 @@ const myVue = new Vue({
         
         },
         stop:function(){
+
             clearInterval(this.time);
             this.time = null;
             this.resetButton = true
+            
         },
         reset:function(){
-            this.totalTime = (25 * 60);
+
+            this.totalTime = (1 * 60);
             clearInterval(this.time);
             this.time = null;
             this.resetButton = false;
